97d888d04daf2e5602fb0c2ff5fa258cf019b49d
Noel Ullreich <noelullreich@proton.me>
2025-09-03 19:14:15 +0200

    updated web_serve file to work with new layout

--- server_queen.py
+++ server_queen.py
@@ -1,3 +1,3 @@
 from textual_serve.server import Server
-server = Server("python -m week_prototype ETH_timetable.ics 16.9.2024")
+server = Server("python -m main ETH_timetable.ics 16.9.2024")
 server.serve()
